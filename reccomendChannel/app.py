de42e56b3b28a46fdc387f798ff82914aad2ab69
seenyou09 <iksunyoo2000@gmail.com>
2024-01-25 06:52:45 -0800

    commit

--- reccomendChannel/app.py
+++ reccomendChannel/app.py
@@ -5,15 +5,18 @@ import pandas as pd
 from yt_channelVideo_stats import yt_channel_vid_statistic
 from googleapiclient.discovery import build
 import trainSimilar as ts 
+from pathlib import Path
+
+
 api_key = "AIzaSyDqYcV4mJWQImAteWnLeIzVgO4X9fO-mIY"
 #api_key = "AIzaSyAQ5NOVTp3LuGQZTb3RC_RREpTe3JGpvdg"
 youtube = build("youtube", "v3", developerKey=api_key)
-
+relative_path = Path('/SimiliarYTchannel-FPVpilot/json/yt_channelVideo_stats-Database.json')
+db_file_path = '/Users/seanyoo/Desktop/SimiliarYTchannel-FPVpilot/json/yt_channelVideo_stats-Database.json'
 
 #Cache csv file 
-@st.cache_data()
-def loadKnn():
-    db_file_path = '/Users/seanyoo/Desktop/recommending_similiarity/json/yt_channelVideo_stats-Database.json'
+@st.cache_data
+def loadKnn(db_file_path):
     with open(db_file_path) as json_file:
         jsondata = json.load(json_file) 
     #Create the similiarity in Database
@@ -38,7 +41,7 @@ options3 = form1.slider('',1,50,5)
 button1 = form1.form_submit_button("submit")
 
 if button1:
-    json_data, db_pd =  loadKnn()
+    json_data, db_pd =  loadKnn(db_file_path)
     input = yt_channel_vid_statistic(option1, api_key, youtube, my_categ="")
     input_dic = input.final_stats()
     input_pd = ts.convertDictPd(input_dic)
